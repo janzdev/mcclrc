f1555a6fcd1dae1e692697f4cbc904cb62091171
jandev <pedroofiasa020501@gmail.com>
2023-05-02 11:40:31 +0800

    update dbcon.php

--- admin/config/dbcon.php
+++ admin/config/dbcon.php
@@ -1,16 +1,13 @@
 <?php 
-<<<<<<< HEAD
-// $host = "localhost";
-// $username = "root";
-// $password = "";
-// $database = "mcclrc";
-
-=======
->>>>>>> a5dc42cdf9ffc138860bc31ee46f6303f9f74c55
 $host = "127.0.0.1";
 $username = "mcclrc";
 $password = "mcclrc";
 $database = "mcclrc";
+
+// $host = "localhost";
+// $username = "root";
+// $password = "";
+// $database = "mcclrc";
 
 $con = mysqli_connect("$host", "$username", "$password", "$database");
 
